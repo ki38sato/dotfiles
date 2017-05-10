f2c61ad6e88e61c56e835e31b7567ac68b47cd48
Kimiya Sato <ki38sato@gmail.com>
2017-05-10 14:32:14 +0900

    Add setup for Mac

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle
+brew bundle
+
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# setup files
+if [ -e $HOME/.zshrc ]; then
+  mv $HOME/.zshrc $HOME/.zshrc.bak
+fi
+cp .zshrc $HOME/.zshrc
+
+if [ -e $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+fi
+cp .tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/ref/src $HOME/dev/src $HOME/dev/docker_home
+
+if [ -f $HOME/.gitconfig ]; then
+  :
+else
+  git config --global --add ghq.root $HOME/ref/src
+  git config --global --add ghq.root $HOME/dev/src
+fi
+
+# setup Atom packages
+apm install --packages-file atom_packages.txt
